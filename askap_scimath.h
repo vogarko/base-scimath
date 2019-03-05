ea93fa044ab9edb2fbb696290cecc3954aa52b65
Stephen Ord <ord006@miranda.atnf.csiro.au>
2019-03-05 16:01:06 +1100

    beginning build attempts

---
+++ askap_scimath.h
@@ -0,0 +1,25 @@
+// This is an automatically generated file. Please DO NOT edit!
+/// @file
+///
+/// Package config file. ONLY include in ".cc" files never in header files!
+
+// std include
+#include <string>
+
+#ifndef ASKAP_SCIMATH_H
+#define ASKAP_SCIMATH_H
+
+  /// The name of the package
+#define ASKAP_PACKAGE_NAME "scimath"
+
+/// askap namespace
+namespace askap {
+  /// @return version of the package
+///  std::string getAskapPackageVersion_scimath();
+}
+
+  /// The version of the package
+//#define ASKAP_PACKAGE_VERSION askap::getAskapPackageVersion_scimath()
+#define ASKAP_PACKAGE_VERSION "0.0.0"
+
+#endif
