b346c119215f98ed3e41d2a1f7817e93299f6f35
Stephen Ord <stephen.ord@csiro.au>
2019-03-05 18:19:40 +1100

    added the LSQR solver to scimath

---
+++ lsqr_solver/GlobalTypedefs.h
@@ -0,0 +1,18 @@
+/*
+ * GlobalTypedefs.h
+ *
+ * @author Vitaliy Ogarko <vogarko@gmail.com>
+ */
+
+#ifndef SRC_GLOBALTYPEDEFS_H_
+#define SRC_GLOBALTYPEDEFS_H_
+
+#include <vector>
+
+namespace askap { namespace lsqr {
+
+    typedef std::vector<double> Vector;
+
+}} // namespace askap.lsqr
+
+#endif /* SRC_GLOBALTYPEDEFS_H_ */
